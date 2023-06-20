27ab3ed9d4ddb9a763ebb2bd6bb8223cc73003ca
pipiipin <74450504+pipiipin@users.noreply.github.com>
2023-06-20 11:09:34 +0700

    Update

---
+++ src/components/mostview/Mostview.jsx
@@ -0,0 +1,67 @@
+'use client'
+import React, { useState, useEffect, useCallback } from 'react'
+import Image from 'next/image'
+import styles from './mostview.module.css'
+import Link from 'next/link'
+
+export default function Mostview({ category, channel, id }) {
+  const [data, setData] = useState([])
+
+  const fetchData = useCallback(async () => {
+    try {
+      let url =
+        'http://localhost:3003/contents/?_limit=3&?_sort=views&_order=desc&'
+      if (category) url += `category=${category}&`
+      if (channel) url += `channel=${channel}&`
+      if (id) url += `id_ne=${id}`
+
+      const response = await fetch(url)
+      const data = await response.json()
+
+      setData(data)
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    }
+  }, [category, channel, id])
+  useEffect(() => {
+    if (category || channel || id) {
+      fetchData()
+    }
+  }, [category, channel, fetchData, id])
+
+  return (
+    <div className={styles.mostview}>
+      <h3>Most viewed</h3>
+      {data.map((item) => (
+        <Link
+          className={styles.mvCard}
+          key={item.id}
+          href={{
+            pathname: `/blog/[id]`,
+          }}
+          as={`/blog/${item.id}`}
+        >
+          <div className={styles.stack}>
+            <Image
+              src={item.image}
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{
+                width: '100%',
+                height: 'auto',
+                borderTopLeftRadius: '10px',
+                borderTopRightRadius: '10px',
+              }}
+              alt="Most Viewed News image"
+            />
+            <div className={styles.view}>
+              <p>{item.views} views</p>
+            </div>
+          </div>
+          <h5 className={styles.mvTitle}>{item.title}</h5>
+        </Link>
+      ))}
+    </div>
+  )
+}
